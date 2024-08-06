a9cb013c8fad67d01d666b3f392a005c6c66ad8e
Jeremy Moon <jeremymoon567@gmail.com>
2024-08-05 21:49:25 -0700

    initial project section working

---
+++ components/Projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='py-20'>
+      <h1 className='heading'>
+        Featured {' '}
+        <span className='text-purple'>Projects</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Projects
