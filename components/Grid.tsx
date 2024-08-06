e1a8aa733a2956ce6aaa335612c1156368115a0b
Jeremy Moon <jeremymoon567@gmail.com>
2024-08-05 21:54:22 -0700

    add heading to grid section

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,6 +5,12 @@ import { gridItems } from '@/data'
 const Grid = () => {
   return (
     <section id="about">
+      <div className='pt-32 pb-12'>
+        <h1 className='heading'>
+          About {' '}
+          <span className='text-purple'>Me</span>
+        </h1>
+      </div>
       <BentoGrid>
         {gridItems.map(
           ({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
