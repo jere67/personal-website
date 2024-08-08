e2f7dc6588d60dc2581132ce28254942a53b95de
Jeremy Moon <jeremymoon567@gmail.com>
2024-08-08 14:04:09 -0700

    fix broken favicon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         <StarsCanvas/>
         <ThemeProvider
