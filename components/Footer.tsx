7aa2c416b38da063711f95bc4bf90c342b565cd2
Jeremy Moon <jeremymoon567@gmail.com>
2024-08-09 09:14:59 -0700

    fix dynamic date in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
       </div>
 
       <div className='flex mt-16 md:flex-row flex-col justify-between items-center z-[20]'>
-        <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Jeremy Moon</p>
+        <p className='md:text-base text-sm md:font-normal font-light'>Copyright © {new Date().getFullYear()} Jeremy Moon</p>
 
         <div className='flex items-center md:gap-3 gap-6 z-[20]'>
           {socialMedia.map((profile) => (
