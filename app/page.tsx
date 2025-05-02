3b7c0462261871c6a5188afc9e65c43958f50c9e
jere67 <jeremymoon567@gmail.com>
2025-05-02 13:41:01 -0700

    add experiences component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Education from "@/components/Education";
+import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -14,6 +15,7 @@ export default function Home() {
       <TracingBeam className="max-w-7xl w-full">
         <Navbar navItems={navItems}/>
         <Hero />
+        <Experience />
         <Grid />
         <Projects />
         <Education />
