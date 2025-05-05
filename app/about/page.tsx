8ff43a4cff47479ef986ba3756dfa82ff81545f3
jere67 <jeremymoon567@gmail.com>
2025-05-05 15:37:26 -0700

    update new navbar on external pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "@/components/Footer";
-import { NavbarExternal } from "@/components/NavbarExternal";
 import React from "react";
 import About from "./About";
+import { Navbar } from "@/components/ui/Navbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <menu>
-      <NavbarExternal />
+      <Navbar navItems={navItems} />
       <div className="container mx-auto px-4 pt-8">
         <About />
         <Footer showContact={false} />
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,12 +3,14 @@ import { NavbarExternal } from "@/components/NavbarExternal";
 import React from "react";
 import { Projects } from "./Projects";
 import { TracingBeam } from "@/components/ui/TracingBeam";
+import { Navbar } from "@/components/ui/Navbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col mx-auto sm:px-10 overflow-clip ">
+    <main className="relative bg-black flex justify-center items-center flex-col mx-auto sm:px-10 overflow-clip">
       <TracingBeam>
-        <NavbarExternal />
+        <Navbar navItems={navItems} />
         <div className="relative container mx-auto px-4 pt-8 z-30">
           <Projects />
           <Footer showContact={false} />
